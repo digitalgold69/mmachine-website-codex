87c15d59160dc24171a6d62079d055d81405a6c0
M-Machine Daily Sync <metals@m-machine.co.uk>
2026-06-25 23:20:47 +0100

    Daily sync 2026-06-25

--- lib/mini-data.ts
+++ lib/mini-data.ts
@@ -10311,8 +10311,8 @@ export const products: Product[] = [
     "bodyType": null,
     "mark": "Mark 1-3",
     "hand": "unhanded",
-    "priceExVat": 99,
-    "priceIncVat": 118.8,
+    "priceExVat": 80,
+    "priceIncVat": 96.0,
     "stock": "in",
     "stockQty": 0,
     "category": "mini"
